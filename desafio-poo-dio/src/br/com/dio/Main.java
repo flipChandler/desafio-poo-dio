89e28bc2cd24ae8dc7bdd7c1d8553e0f1dc10753
flipChandler <felipe.santos11@hotmail.com>
2021-12-14 02:13:39 -0300

    feat execuçao das regras de negocios

--- desafio-poo-dio/src/br/com/dio/Main.java
+++ desafio-poo-dio/src/br/com/dio/Main.java
@@ -2,8 +2,10 @@ package br.com.dio;
 
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -30,8 +32,38 @@ public class Main {
 		System.out.println(curso2);
 		System.out.println(mentoria);
 		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev dev1 = new Dev();
+		dev1.setNome("Camila");
+		dev1.inscreverBootcamp(bootcamp);
+		System.out.println("\nConteúdos incritos de Camila: " + dev1.getConteudosInscritos());
+		
+		dev1.progredir();
+		dev1.progredir();
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("Conteúdos incritos de Camila: " + dev1.getConteudosInscritos());
+		System.out.println("XP: " + dev1.calculaTotalXp());
+		System.out.println("Conteúdos concluídos de Camila: " + dev1.getConteudosConcluidos() + "\n");
 		
 		
+		Dev dev2 = new Dev();
+		dev2.setNome("Felipe");
+		dev2.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos incritos de Felipe: " + dev2.getConteudosInscritos());
+		
+		dev2.progredir();
+		dev2.progredir();
+		dev2.progredir();
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("Conteúdos incritos de Felipe: " + dev2.getConteudosInscritos());
+		System.out.println("XP: " + dev2.calculaTotalXp());
+		System.out.println("Conteúdos concluídos de Felipe: " + dev2.getConteudosConcluidos());
 	}
 
 }
